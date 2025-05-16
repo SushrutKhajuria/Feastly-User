1911d7adb8aec85652e7599af36b7936d5298cc6
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-05-16 13:47:01 +0530

    Redux Store Setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
+import { Provider } from "react-redux";
+import store from "./store"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <Provider store={store}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Provider>
 );
